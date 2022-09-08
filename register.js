820dfa82beb7a0297c5bd4ddf0d883b646fdfd61
Anggelitozz <98678182+Anggelitozz@users.noreply.github.com>
2022-09-07 20:41:46 -0500

    Add files via upload

--- register.js
+++ register.js
@@ -1,3 +1,5 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
@@ -6,3 +8,38 @@ const expresiones = {
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
+
+const validarfomulario = (e) => {
+	switch(e.target.name){
+		case "username":
+			if(expresiones.usuario.test(e.target.value)){
+				console.log('yeah')
+				document.getElementById('usuario').classList.add('.controls_incorrecto')
+
+			}else{
+			}
+		break;
+		case "usuario":
+
+		break;
+		case "usuario":
+
+		break;
+		case "usuario":
+
+		break;
+		case "usuario":
+
+		break;
+
+	}
+};
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarfomulario);
+	input.addEventListener('blur', validarfomulario);
+})
+
+formulario.addEventListener('submit',(e) => {
+	e.preventDefault();
+});
