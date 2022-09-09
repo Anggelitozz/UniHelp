84094e6414512039f30baac781f0ecf76ce598b1
anggelitozz <ortegagarcia112015@gmail.com>
2022-09-09 09:06:31 -0500

    actualizacion

--- register.js
+++ register.js
@@ -6,38 +6,27 @@ const expresiones = {
 	nombres: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	apellidos: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
 
-const validarfomulario = (e) => {
+const validarformulario = (e) => {
 	switch(e.target.name){
 		case "username":
-			if(expresiones.usuario.test(e.target.value)){
-				console.log('yeah')
-				document.getElementById('usuario').classList.add('.controls_incorrecto')
-
+			if(expresiones.usuario.test(e.target.value)==true){
+				document.getElementById('usuario').classList.remove('controls_incorrecto');
+				console.log('correcto')
 			}else{
+				document.getElementById('usuario').classList.add('controls_incorrecto');
+
 			}
 		break;
-		case "usuario":
-
-		break;
-		case "usuario":
-
-		break;
-		case "usuario":
-
-		break;
-		case "usuario":
-
-		break;
-
 	}
+
 };
 
 inputs.forEach((input) => {
-	input.addEventListener('keyup', validarfomulario);
-	input.addEventListener('blur', validarfomulario);
+	input.addEventListener('keyup', validarformulario);
+	input.addEventListener('blur', validarformulario);
 })
 
 formulario.addEventListener('submit',(e) => {
