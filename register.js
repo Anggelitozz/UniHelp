91f88fc6ac37d8891068b073cb0b7e9c168974b4
Anggelitozz <98678182+Anggelitozz@users.noreply.github.com>
2022-09-11 16:23:18 -0500

    Add files via upload

--- register.js
+++ register.js
@@ -11,75 +11,36 @@ const expresiones = {
 const validarformulario = (e) => {
 	switch(e.target.name){
 		case "username":
-			if(expresiones.usuario.test(e.target.value)==true){
-				document.getElementById('usuario').classList.remove('controls_incorrecto');
-				document.getElementById('usuario').classList.add('controls');
-				document.getElementById('usuarioError').classList.remove('activarMensaje');
-			}else{
-				document.getElementById('usuario').classList.add('controls_incorrecto');
-				document.getElementById('usuarioError').classList.add('activarMensaje');
-				document.getElementById('nombresError').classList.remove('error');
-
-			}
+			validar(expresiones.usuario, e.target, e.target.name);
 		break;
 		case "nombres":
-			if(expresiones.nombres.test(e.target.value)==true){
-				document.getElementById('nombres').classList.remove('controls_incorrecto');
-				document.getElementById('nombres').classList.add('controls');
-				document.getElementById('nombresError').classList.remove('activarMensaje');
-			}else{
-				document.getElementById('nombres').classList.add('controls_incorrecto');
-				document.getElementById('nombresError').classList.add('activarMensaje');
-				document.getElementById('nombresError').classList.remove('error');
-			}
+			validar(expresiones.nombres, e.target, e.target.name);
 		break;
 		case "apellidos":
-			if(expresiones.nombres.test(e.target.value)==true){
-				document.getElementById('apellidos').classList.remove('controls_incorrecto');
-				document.getElementById('apellidos').classList.add('controls');
-				document.getElementById('usuarioError').classList.remove('activarMensaje');
-			}else{
-				document.getElementById('apellidos').classList.add('controls_incorrecto');
-				document.getElementById('apellidosError').classList.add('activarMensaje');
-
-			}
+			validar(expresiones.nombres, e.target, e.target.name);
 		break;
 		case "email":
-			if(expresiones.correo.test(e.target.value)==true){
-				document.getElementById('email').classList.remove('controls_incorrecto');
-				document.getElementById('email').classList.add('controls');
-				document.getElementById('usuarioError').classList.remove('activarMensaje');
-			}else{
-				document.getElementById('email').classList.add('controls_incorrecto');
-				document.getElementById('emailError').classList.add('activarMensaje');
-
-			}
+			validar(expresiones.correo, e.target, e.target.name);
 		break;
 		case "contraseña":
-			if(expresiones.password.test(e.target.value)==true){
-				document.getElementById('contraseña').classList.remove('controls_incorrecto');
-				document.getElementById('contraseña').classList.add('controls');
-				document.getElementById('usuarioError').classList.remove('activarMensaje');
-			}else{
-				document.getElementById('contraseña').classList.add('controls_incorrecto');
-				document.getElementById('contraseñaError').classList.add('activarMensaje');
-
-			}
+			validar(expresiones.password, e.target, e.target.name);
 		break;
 		case "contraseña2":
-			if(expresiones.password.test(e.target.value)==true){
-				document.getElementById('contraseña2').classList.remove('controls_incorrecto');
-				document.getElementById('contraseña2').classList.add('controls');
-				document.getElementById('usuarioError').classList.remove('activarMensaje');
-			}else{
-				document.getElementById('contraseña2').classList.add('controls_incorrecto');
-				document.getElementById('contraseña2Error').classList.add('activarMensaje');
-
-			}
+			validar(expresiones.usuario, e.target, e.target.name);
 		break;
 	}
 
 };
+const validar = (exp,input,id) =>{
+	if(exp.test(input.value)==true){
+		document.getElementById(`${id}`).classList.remove('controls_incorrecto');
+		document.getElementById(`${id}Error`).classList.remove('activarMensaje');
+	}else{
+		document.getElementById(`${id}`).classList.add('controls_incorrecto');
+		document.getElementById(`${id}Error`).classList.add('activarMensaje');
+		document.getElementById(`${id}Error`).classList.remove('error');
+	}
+}
 
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarformulario);
