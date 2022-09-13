d8884c69391498722e5540bd4921e5f33f8044e8
anggelitozz <ortegagarcia112015@gmail.com>
2022-09-13 02:36:28 -0500

    new file:   home.css 	new file:   home.html 	modified:   index.css 	modified:   index.html 	modified:   login.css 	modified:   login.html 	new file:   logo.jpg 	modified:   register.css 	modified:   register.html 	modified:   register.js

--- register.js
+++ register.js
@@ -2,44 +2,72 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	usuario: /^[a-zA-Z0-9\_\-]{1,16}$/, // Letras, numeros, guion y guion_bajo
 	nombres: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 }
 
+const campos = {
+	usuario: false,
+	nombres: false,
+	apellidos: false,
+	email: false,
+	contraseña: false
+}
+
 const validarformulario = (e) => {
 	switch(e.target.name){
 		case "username":
-			validar(expresiones.usuario, e.target, e.target.name);
+			validar(expresiones.usuario, e.target, 'usuario');
 		break;
 		case "nombres":
-			validar(expresiones.nombres, e.target, e.target.name);
+			validar(expresiones.nombres, e.target, 'nombres');
 		break;
 		case "apellidos":
-			validar(expresiones.nombres, e.target, e.target.name);
+			validar(expresiones.nombres, e.target, 'apellidos');
 		break;
 		case "email":
-			validar(expresiones.correo, e.target, e.target.name);
+			validar(expresiones.correo, e.target, 'email');
 		break;
 		case "contraseña":
-			validar(expresiones.password, e.target, e.target.name);
+			validar(expresiones.password, e.target, 'contraseña');
+			validarPassword();
 		break;
 		case "contraseña2":
-			validar(expresiones.usuario, e.target, e.target.name);
+			validarPassword();
 		break;
 	}
 
-};
+}
 const validar = (exp,input,id) =>{
-	if(exp.test(input.value)==true){
+	if(exp.test(input.value)){
 		document.getElementById(`${id}`).classList.remove('controls_incorrecto');
 		document.getElementById(`${id}Error`).classList.remove('activarMensaje');
+		document.getElementById(`${id}Error`).classList.add('error');
+		campos[id]=true;
 	}else{
 		document.getElementById(`${id}`).classList.add('controls_incorrecto');
 		document.getElementById(`${id}Error`).classList.add('activarMensaje');
 		document.getElementById(`${id}Error`).classList.remove('error');
+		campos[id]=false;
 	}
+}
+const validarPassword =() =>{
+	const inputPassword = document.getElementById('contraseña') 
+	const inputPassword2 = document.getElementById('contraseña2') 
+	if(inputPassword.value!==inputPassword2.value){
+		document.getElementById('contraseña2').classList.add('controls_incorrecto');
+		document.getElementById('contraseña2Error').classList.add('activarMensaje');
+		document.getElementById('contraseña2Error').classList.remove('error');
+		campos['contraseña']=false;
+	}else{
+		document.getElementById('contraseña2').classList.remove('controls_incorrecto');
+		document.getElementById('contraseña2Error').classList.remove('activarMensaje');
+		document.getElementById('contraseña2Error').classList.add('error');
+		campos['contraseña']=true;
+	}
+
 }
 
 inputs.forEach((input) => {
@@ -48,5 +76,13 @@ inputs.forEach((input) => {
 })
 
 formulario.addEventListener('submit',(e) => {
+	const term = document.getElementById('term')
+	if(campos.usuario && campos.nombres && campos.apellidos && campos.email && campos.contraseña && term.checked){
+		formulario.reset();
+		document.getElementById('correcto').classList.add('activarMensajeCorrecto');
+	}
+	else{
+		document.getElementById('incorrecto').classList.add('activarMensaje');
+	}
 	e.preventDefault();
-});
+})
